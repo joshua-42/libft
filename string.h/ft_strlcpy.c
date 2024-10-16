79dce69515322ca8138e123e0a9238ff32bc2807
Joshua Douglass <jodougla@3E6.42angouleme.fr>
2024-10-16 23:55:58 +0200

    clear

---
+++ string.h/ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodougla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 16:25:11 by jodougla          #+#    #+#             */
+/*   Updated: 2024/10/13 04:37:31 by jodougla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	const char		*tmp_src;
+	size_t			i;
+
+	tmp_src = src;
+	i = 0;
+	if (size)
+	{
+		while (*tmp_src && i++ < (size - 1))
+			*dest++ = *tmp_src++;
+		*dest = 0;
+	}
+	i = ft_strlen(src);
+	return (i);
+}
---
+++ string.h/ft_strrchr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodougla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 18:04:20 by jodougla          #+#    #+#             */
+/*   Updated: 2024/10/09 13:45:58 by jodougla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	unsigned char	*tmp_s;
+
+	tmp_s = NULL;
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			tmp_s = (unsigned char *)s;
+		s++;
+	}
+	if (!(unsigned char)c)
+		return ((char *)s);
+	return ((char *)tmp_s);
+}
